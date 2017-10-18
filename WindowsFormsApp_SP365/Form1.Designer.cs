a4f6667b8c592477f21959296558872b1aaadeeb
Oleg Dubetsky <oleg.dubetsky@gmail.com>
2017-10-18 18:30:29 +0300

    Add project files.

---
+++ WindowsFormsApp_SP365/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApp_SP365
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lWeb = new System.Windows.Forms.Label();
+            this.tbSiteURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btLoadSite = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbPwd = new System.Windows.Forms.TextBox();
+            this.lbLists = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbViews = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lWeb
+            // 
+            this.lWeb.AutoSize = true;
+            this.lWeb.Location = new System.Drawing.Point(280, 58);
+            this.lWeb.Name = "lWeb";
+            this.lWeb.Size = new System.Drawing.Size(63, 13);
+            this.lWeb.TabIndex = 1;
+            this.lWeb.Text = "Not opened";
+            // 
+            // tbSiteURL
+            // 
+            this.tbSiteURL.Location = new System.Drawing.Point(69, 27);
+            this.tbSiteURL.Name = "tbSiteURL";
+            this.tbSiteURL.Size = new System.Drawing.Size(201, 20);
+            this.tbSiteURL.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Site URL";
+            // 
+            // btLoadSite
+            // 
+            this.btLoadSite.Location = new System.Drawing.Point(276, 25);
+            this.btLoadSite.Name = "btLoadSite";
+            this.btLoadSite.Size = new System.Drawing.Size(70, 23);
+            this.btLoadSite.TabIndex = 4;
+            this.btLoadSite.Text = "Load";
+            this.btLoadSite.UseVisualStyleBackColor = true;
+            this.btLoadSite.Click += new System.EventHandler(this.btLoadSite_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Login";
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(69, 53);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(201, 20);
+            this.tbLogin.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "pwd";
+            // 
+            // tbPwd
+            // 
+            this.tbPwd.Location = new System.Drawing.Point(69, 79);
+            this.tbPwd.Name = "tbPwd";
+            this.tbPwd.PasswordChar = '*';
+            this.tbPwd.Size = new System.Drawing.Size(201, 20);
+            this.tbPwd.TabIndex = 7;
+            // 
+            // lbLists
+            // 
+            this.lbLists.FormattingEnabled = true;
+            this.lbLists.Location = new System.Drawing.Point(16, 132);
+            this.lbLists.Name = "lbLists";
+            this.lbLists.Size = new System.Drawing.Size(154, 186);
+            this.lbLists.TabIndex = 9;
+            this.lbLists.DoubleClick += new System.EventHandler(this.lbLists_DoubleClick);
+            this.lbLists.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lbLists_MouseDoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Lists";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(189, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Views";
+            // 
+            // lbViews
+            // 
+            this.lbViews.FormattingEnabled = true;
+            this.lbViews.Location = new System.Drawing.Point(192, 132);
+            this.lbViews.Name = "lbViews";
+            this.lbViews.Size = new System.Drawing.Size(154, 186);
+            this.lbViews.TabIndex = 11;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 329);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(330, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 364);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbViews);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbLists);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbPwd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.btLoadSite);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSiteURL);
+            this.Controls.Add(this.lWeb);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "O365 SharePoint List Items Updator from Excel Data";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lWeb;
+        private System.Windows.Forms.TextBox tbSiteURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btLoadSite;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbPwd;
+        private System.Windows.Forms.ListBox lbLists;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lbViews;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ WindowsFormsApp_SP365/Form1.cs
@@ -0,0 +1,252 @@
+﻿using Microsoft.SharePoint.Client;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SP = Microsoft.SharePoint.Client;
+
+using System.IO;
+using System.Xml;
+
+namespace WindowsFormsApp_SP365
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        List<AllList> Lists = new List<AllList>();
+        List<AllView> Views = new List<AllView>();
+        MsOnlineClaimsHelper claimsHelper;
+        ClientContext clientContext;
+        Web oWebsite;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void btOpenList_Click(object sender, EventArgs e)
+        {
+           
+
+           
+
+        }
+
+        private void btLoadSite_Click(object sender, EventArgs e)
+        {
+            claimsHelper = new MsOnlineClaimsHelper(tbSiteURL.Text, tbLogin.Text, tbPwd.Text);
+            clientContext = new ClientContext(tbSiteURL.Text);
+           
+                clientContext.ExecutingWebRequest += claimsHelper.clientContext_ExecutingWebRequest;
+
+                oWebsite = clientContext.Web;
+                ListCollection collList = oWebsite.Lists;
+            
+             
+                clientContext.Load(collList);
+
+                clientContext.ExecuteQuery();
+
+
+                clientContext.Load(oWebsite);
+                clientContext.ExecuteQuery();
+                lWeb.Text = oWebsite.Title;
+               
+                foreach (SP.List oList in collList)
+                {
+                    Lists.Add(new AllList { Title = oList.Title, Id = oList.Id });
+
+                    lbLists.Items.Add(oList.Title);
+                }
+            
+        }
+
+        private void lbLists_DoubleClick(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void lbLists_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var sList = Lists.Where(x => x.Title == lbLists.SelectedItem.ToString());
+            //Get view
+            //Views.Clear();
+            List list = clientContext.Web.Lists.GetByTitle(sList.First().Title.ToString());
+            //ViewCollection viewColl = list.Views;
+            //clientContext.Load(viewColl,
+            //    views => views.Include(
+            //        view => view.Title,
+            //        view => view.Id));
+            //clientContext.ExecuteQuery();
+            //foreach (SP.View view in viewColl)
+            //{
+              
+            //    lbViews.Items.Add(view.Title);
+            //    Views.Add(new AllView { Name = view.Title, Id = view.Id });
+            //}
+
+
+            SP.List oList = clientContext.Web.Lists.GetByTitle(sList.First().Title.ToString());
+            var camlQuery = new CamlQuery { ViewXml = "<View><RowLimit>100000</RowLimit></View>" };
+            ListItemCollection collListItem = oList.GetItems(camlQuery);
+
+            clientContext.Load(collListItem,
+             items => items.Include(
+                item => item.Id,
+                item => item.DisplayName,
+                item => item.HasUniqueRoleAssignments));
+
+            clientContext.ExecuteQuery();
+
+          //  MessageBox.Show("In list "+sList.First().Id.ToString()+Environment.NewLine+"Found items: "+ collListItem.Count);
+
+            DialogResult dialogResult = MessageBox.Show("Really delete "+ collListItem.Count+" items?", "Deleting items list "+ sList.First().Title.ToString(), MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                //Delete items
+                CamlQuery q = new CamlQuery();
+                q.ViewXml = "<View><RowLimit>10000</RowLimit></View>";
+                // We get the results
+                SP.ListItemCollection coll = list.GetItems(q);
+
+                clientContext.Load(coll);
+                clientContext.ExecuteQuery();
+
+
+                if (coll.Count > 0)
+                {
+                    for (int i = coll.Count - 1; i >= 0; i--)
+                    {
+                        coll[i].DeleteObject();
+
+                        progressBar1.Value = (int)(i/collListItem.Count  ) * 100;
+                        int percent = (int)(((double)progressBar1.Value / (double)progressBar1.Maximum) * 100);
+                        progressBar1.Refresh();
+                        progressBar1.CreateGraphics().DrawString(percent.ToString() + "%",
+                            new Font("Arial", (float)8.25, FontStyle.Regular),
+                            Brushes.Black,
+                            new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));
+                    }
+                    clientContext.ExecuteQuery();
+                }
+                MessageBox.Show("Done!");
+                progressBar1.Value = 0;
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+                DialogResult dialogResult2 = MessageBox.Show("Import rows?", "Import from Excel in list " + sList.First().Title.ToString(), MessageBoxButtons.YesNo);
+                if (dialogResult2 == DialogResult.Yes)
+                {
+                    // Create an instance of the open file dialog box.
+                    OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+                    // Set filter options and filter index.
+                    openFileDialog1.Filter = "Excel File (.xlsx)|*.xlsx";
+                    openFileDialog1.FilterIndex = 1;
+
+                    openFileDialog1.Multiselect = false;
+
+                    // Call the ShowDialog method to show the dialog box.
+                    if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        try
+                        {
+                            var bookFileNULL = new LinqToExcel.ExcelQueryFactory(openFileDialog1.FileName);
+                            List<AllItem>PoliciesNULL =
+                            (from row in bookFileNULL.Worksheet("Table1")
+                            let item = new AllItem
+                            {
+                                Name = row["Title"].Cast<string>()
+                            }
+                            // where item.Supplier == "Walmart"
+                            select item).ToList();
+
+                            MessageBox.Show("Found items for import: " + PoliciesNULL.Count());
+                            //Add items
+                            int i = 0;
+                            foreach(AllItem pol in PoliciesNULL)
+                            {
+                                i++;
+                                AddItems(sList.First().Title.ToString(), Views.First().Id.ToString(), pol);
+
+                                progressBar1.Value = (int)(i/ PoliciesNULL.Count)*100;
+                                int percent = (int)(((double)progressBar1.Value / (double)progressBar1.Maximum) * 100);
+                                progressBar1.Refresh();
+                                progressBar1.CreateGraphics().DrawString(percent.ToString() + "%",
+                                    new Font("Arial", (float)8.25, FontStyle.Regular),
+                                    Brushes.Black,
+                                    new PointF(progressBar1.Width / 2 - 10, progressBar1.Height / 2 - 7));
+                            }
+                            MessageBox.Show("Done!");
+                            progressBar1.Value = 0;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                        }
+                        
+                    }
+                }
+                else if (dialogResult2 == DialogResult.No)
+                {
+                    //do something else
+                }
+            }
+        }
+
+        private static String BuildBatchDeleteCommand(SP.List list, SP.ListItemCollection coll)
+        {
+            StringBuilder sbDelete = new StringBuilder();
+            sbDelete.Append("<?xml version=\"1.0\" encoding=\"UTF-8\"?><Batch>");
+
+            // We prepare a String.Format with a String.Format, this is why we have a {{0}}
+            string command = String.Format("<Method><SetList Scope=\"Request\">{0}</SetList><SetVar Name=\"ID\">{{0}}</SetVar><SetVar Name=\"Cmd\">Delete</SetVar></Method>", list.Id);
+            foreach (SP.ListItem item in coll)
+            {
+                sbDelete.Append(string.Format(command, item.Id.ToString()));
+            }
+            sbDelete.Append("</Batch>");
+            return sbDelete.ToString();
+        }
+
+        private void AddItems(string listGUID, string activeItemViewGUID, AllItem item)
+        {
+            var list = clientContext.Web.Lists.GetByTitle(listGUID);
+            var itemCreateInfo = new ListItemCreationInformation();
+            var listItem = list.AddItem(itemCreateInfo);
+            listItem["Title"] = item.Name;
+            listItem.Update();     //prepare to create new List Item
+            clientContext.ExecuteQuery();   //submit request to the server goes here   
+        }
+    }
+
+
+
+    internal class AllList
+    {
+        public String Title { get; set; }
+        public Guid Id { get; set; }
+    }
+
+    internal class AllView
+    {
+        public String Name { get; set; }
+        public Guid Id { get; set; }
+    }
+
+    internal class AllItem
+    {
+        public String Name { get; set; }
+       
+    }
+}
